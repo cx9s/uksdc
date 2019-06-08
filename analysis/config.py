5b90bcc001cc4f4a0c9a2aec09728fcd3f6b3a75
cx9s <cx9918@gmail.com>
2019-06-08 17:36:22 +0100

    delete adv

--- analysis/config.py
+++
@@ -1,9 +0,0 @@
-
-INPUT_FILE = '/Users/xuanchen/workplace/ldbj/analysis/20190121.tsv'
-
-OUTPUT_FEE = 'fee.json'
-OUTPUT_USERS = 'player.json'
-
-LOCATION_OUTPUT_FILE = 'location.json'
-
-SPLIT_FLAG = '\t'
